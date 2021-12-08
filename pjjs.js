ef9e44d333bafb2a474946b91126e6f03ab878f4
danh9299 <nguyenduyanh.tit@gmail.com>
2021-12-08 22:27:34 +0700

    update file

---
+++ pjjs.js
@@ -0,0 +1,9 @@
+
+
+let determine = document.getElementsByClassName(determine);
+
+determine.addEventListener("click",Func1);
+
+function Func1(){
+    pic.innerHTML= 'img/cute-ufo-and-alien-cursor-pack.png';
+}
